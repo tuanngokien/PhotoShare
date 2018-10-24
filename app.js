5a46e44df25710af0f9a50912cdd2df6d9f868bc
Tuan Ngo Kien <tuanngokien@gmail.com>
2018-10-25 00:29:41 +0700

    [fix] cors

--- app.js
+++ app.js
@@ -6,13 +6,16 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const indexRouter = require('./routes/index');
 const app = express();
+const cors = require('cors');
 
 require('./auth');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+
 app.use(logger('dev'));
+app.use(cors);
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
