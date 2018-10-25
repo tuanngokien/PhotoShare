f413f648816cdad6269de80a110c8b07249a361c
Tuan Ngo Kien <tuanngokien@gmail.com>
2018-10-25 16:30:45 +0700

    [fix] cors

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.set('view engine', 'pug');
 
 
 app.use(logger('dev'));
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
