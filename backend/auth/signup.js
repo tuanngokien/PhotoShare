4a16382867c222cd9362afc878c21122221e60f0
Tuan Ngo Kien <tuanngokien@gmail.com>
2018-10-31 10:46:03 +0700

    [fix] promise warning

--- backend/auth/signup.js
+++ backend/auth/signup.js
@@ -7,22 +7,27 @@ passport.use('signup', new localStrategy({
     passwordField: 'password',
     passReqToCallback: true,
 }, (req, email, password, done) => {
-    User.findOne({where: {email}}).then(user => {
-        if (user) {
-            return done({email: "User Already Exists"}, false);
-        } else {
-            User.create({...req.body, email, password}).then(user => {
-                return done(null, user);
-            }).catch(validationErrors => {
-                let errors = {};
-                for (let e of validationErrors["errors"]) {
-                    errors[e.path] = e.message;
-                }
-                return done(errors, false);
-            });
+    User.findOne({where: {email}})
+        .then(user => {
+            if (user) {
+                return done({email: "User Already Exists"}, false);
+            } else {
+                User.create({...req.body, email, password})
+                    .then(user => {
+                        return done(null, user);
+                    }).catch(validationErrors => {
+                        let errors = {};
+                        for (let e of validationErrors["errors"]) {
+                            errors[e.path] = e.message;
+                        }
+                        return done(errors, false);
+                    }
+                );
+            }
+            return null;
+        }).catch(err => {
+            console.log(err);
+            return done({signup: "Error in signup"}, false);
         }
-    }).catch(err => {
-        console.log(err);
-        return done({signup: "Error in signup"}, false);
-    })
+    );
 }));
