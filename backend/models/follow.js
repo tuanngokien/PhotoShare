a08b5cb9bfacc62ba672562254609b0aee6bd471
minhphuong99 <nguyenminhphuong@unimarmores.com>
2018-11-12 23:34:53 +0700

    fix follow

--- backend/models/follow.js
+++ backend/models/follow.js
@@ -1,11 +1,15 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const follow = sequelize.define('follow', {
-    follow_by: DataTypes.BIGINT,
-    follow_to: DataTypes.BIGINT
+    follow_by: DataTypes.INTEGER,
+    follow_to: DataTypes.INTEGER
   }, {});
   follow.associate = function (models) {
     follow.belongsTo(models.User, { foreignKey: { allowNull: false }});
   };
+  follow.prototype.toJSON = function() {
+    let data = this.dataValues;
+    return data;
+  }
   return follow;
 };
--- backend/routes/follow.js
+++ backend/routes/follow.js
@@ -1,53 +1,54 @@
 const router = require('express').Router();
-const {follow} = require('../models');
+const { follow } = require('../models');
 
 
 router.route('/')
   .post((req, res) => {
-    let {follow_by, follow_to} = req.body;
-    
+    let { follow_to } = req.body;
+    let userId = req.user.id
+
     follow.findOrCreate({
       where: {
-        follow_by: follow_by,
+        follow_by: userId,
         follow_to: follow_to
       }
     }).spread((user, create) => {
-      if(create) {
-        res.status(200).json({name: "SUCCESS", message: "Follow success", data: user});
+      if (create) {
+        res.status(200).json({ name: "SUCCESS", message: "Follow success", data: user });
       }
       else {
-        res.status(400).json({name: "FAIL", message: "Can't follow"});
+        res.status(400).json({ name: "FAIL", message: "Can't follow" });
       }
     }).catch(err => {
-      res.status(500).json({name: "ERROR", message: err})
+      res.status(500).json({ name: "ERROR", message: err })
     })
   })
 
-router.route('/info')
+router.route('/info/:userId')
   .get((req, res, next) => {
-    let follow_by = req.query['follow_by'];
-    let follow_to = req.query['follow_to'];
+    let friendId = req.param.userId;
+    let userId = req.user.id;
 
     follow.count({
       where: {
-        follow_by: follow_by,
-        follow_to: follow_to
+        follow_by: userId,
+        follow_to: friendId
       }
     }).then(isFollow => {
-      if(isFollow) {
-        res.status(200).json({name: "FOLLOWED", message: "followed"})
+      if (isFollow) {
+        res.status(200).json({ name: "FOLLOWED", message: "followed" })
       }
       else {
-        res.status(200).json({name: "NOT_FOLLOWED", message: "not followed"})
+        res.status(200).json({ name: "NOT_FOLLOWED", message: "not followed" })
       }
     }).catch(err => {
-      res.status(500).json({name: "ERROR", message: err})
+      res.status(500).json({ name: "ERROR", message: err })
     })
   })
 
 router.route('/unfollow')
   .delete((req, res) => {
-    let {follow_by, follow_to} = req.body;
+    let { follow_by, follow_to } = req.body;
 
     follow.destroy({
       where: {
@@ -55,15 +56,34 @@ router.route('/unfollow')
         follow_to: follow_to
       }
     }).then(isUnfollw => {
-      if(isUnfollw) {
-        res.status(200).json({name: "SUCCESS", message: "Unfollow success"})
+      if (isUnfollw) {
+        res.status(200).json({ name: "SUCCESS", message: "Unfollow success" })
       }
       else {
-        res.status(400).json({name: "ERROR", message: "Something wrong"})
+        res.status(400).json({ name: "ERROR", message: "Something wrong" })
       }
     }).catch(err => {
-      res.status(500).json({name: "ERROR", message: err})
+      res.status(500).json({ name: "ERROR", message: err })
     })
   })
 
-  module.exports = router;
+router.route('/followed')
+  .get((req, res) => {
+    let userId = req.user.id;
+
+    follow.findAll({
+      where: {
+        follow_by: userId
+      }
+    }).then(follows => {
+      if(follows) {
+        follows = follows.map(follow => follow.toJSON());
+      }
+      res.status(200).json({name: "SUCCESS", follows: follows})
+    })
+    .catch(err => {
+      res.status(500).json({name: "ERROR", message: "Server error"})
+    })
+  })
+
+module.exports = router;
